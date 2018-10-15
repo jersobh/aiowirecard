2e8dadc428c051813bce891f6a6e956d5c6230fa
Jefferson de Andrade Santos <jeffersonsantos@lnxcit020076.cit>
2018-10-15 20:27:28 -0300

    initial

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Moip API wrapper for asyncio/aiohttp",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jersobh/async-moip",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
