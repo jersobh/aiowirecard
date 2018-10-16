ce32e9ec52dcc270a085a24d72f806b45c1a5eb3
Jefferson de Andrade Santos <jeffersonsantos@lnxcit020076.cit>
2018-10-15 23:28:58 -0300

    Documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="async_moip",
-    version="0.0.1",
+    version="0.0.2",
     author="jersobh",
     author_email="jersobh@gmail.com",
     description="Moip API wrapper for asyncio/aiohttp",
